5daa8e165236e95756ec8813db67fb7cd43af6fe
ZIUTY-TTPro <tomasz.zietkowski@gmail.com>
2026-08-28 08:33:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,79 @@
+const CACHE_NAME = 'flr-points-v1.0.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+const offlineFallbackPage = './index.html';
+
+// ==========================
+// INSTALL
+// ==========================
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// ==========================
+// ACTIVATE
+// ==========================
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// ==========================
+// FETCH SUPPORT
+// ==========================
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          if (event.request.url.startsWith('http')) {
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, copy);
+            });
+          }
+          return response;
+        })
+        .catch(async () => {
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request).catch(() => {
+          return caches.match(offlineFallbackPage);
+        });
+      })
+    );
+  }
+});
+
+// ==========================
+// MESSAGE HANDLER
+// ==========================
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
